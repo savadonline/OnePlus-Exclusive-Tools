4bbb236baec50f0041bc6db8d7336352b57caf54
Savad Anjillath <savadonline@gmail.com>
2017-02-19 12:41:00 +0530

    Main Form Created.

--- OnePlus-Exclusive-Tools/frmMain.Designer.cs
+++ OnePlus-Exclusive-Tools/frmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace OnePlus_Exclusive_Tools
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "frmMain";
+            this.Text = "OnePlus Exclusive Tools";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
