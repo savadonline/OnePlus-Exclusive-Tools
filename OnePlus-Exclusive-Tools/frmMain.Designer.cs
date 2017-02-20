4e81df7de5ec85f996442b302ac4bb326e909c21
savadonline <savadonline@gmail.com>
2017-02-20 23:45:22 +0530

    Done with First Commands.

--- OnePlus-Exclusive-Tools/frmMain.Designer.cs
+++ OnePlus-Exclusive-Tools/frmMain.Designer.cs
@@ -28,20 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCheckADB = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnCheckADB
+            // 
+            this.btnCheckADB.Location = new System.Drawing.Point(12, 12);
+            this.btnCheckADB.Name = "btnCheckADB";
+            this.btnCheckADB.Size = new System.Drawing.Size(86, 23);
+            this.btnCheckADB.TabIndex = 0;
+            this.btnCheckADB.Text = "Check ADB";
+            this.btnCheckADB.UseVisualStyleBackColor = true;
+            this.btnCheckADB.Click += new System.EventHandler(this.btnCheckADB_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(449, 284);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCheckADB);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "OnePlus Exclusive Tools";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCheckADB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- OnePlus-Exclusive-Tools/frmMain.cs
+++ OnePlus-Exclusive-Tools/frmMain.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RegawMOD.Android;
 
 namespace OnePlus_Exclusive_Tools
 {
@@ -15,6 +16,41 @@ namespace OnePlus_Exclusive_Tools
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        AndroidController android;
+        Device device;
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //Usually, you want to load this at startup, may take up to 5 seconds to initialize/set up resources/start server
+            android = AndroidController.Instance;
+        }
+
+        private void btnCheckADB_Click(object sender, EventArgs e)
+        {
+            string serial;
+
+            //Always call UpdateDeviceList() before using AndroidController on devices to get the most updated list
+            android.UpdateDeviceList();
+
+            if (android.HasConnectedDevices)
+            {
+                serial = android.ConnectedDevices[0];
+                device = android.GetConnectedDevice(serial);
+                this.label1.Text = device.SerialNumber;
+                this.label2.Text = device.Battery.Level.ToString();
+            }
+            else
+            {
+                this.label1.Text = "Error - No Devices Connected";
+            }
+        }
+
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //ALWAYS remember to call this when you're done with AndroidController.  It removes used resources
+            android.Dispose();
         }
     }
 }
